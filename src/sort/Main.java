b479856cb16e241da4ac00df2bb40489d759f4d4
lucifer <psinghal4210@gmail.com>
2018-12-02 14:05:35 +0530

    Implement Quick Sort

--- src/sort/Main.java
+++ src/sort/Main.java
@@ -2,13 +2,13 @@ package sort;
 
 public class Main {
     public static void main(String[] args){
-        int n = 100;
+        int n = 10000;
         int[] input = new int[n];
         for(int i = input.length - 1 ; i >= 0; i--){
             input[i] = n-1-i;
         }
         //input = new int[]{20, 35, -15, 7, 55, 1, -22,-16,29};
-        MergeSort sorter = new MergeSort(input);
+        QuickSort sorter = new QuickSort(input);
         sorter.sort();
 
         sorter.print();
---
+++ src/sort/QuickSort.java
@@ -0,0 +1,58 @@
+package sort;
+
+public class QuickSort {
+    private int[] data;
+    private int start;
+    private int end;
+    private int length;
+
+    QuickSort(int[] input){
+        data = input;
+        start = 0;
+        end = data.length;
+        length = data.length;
+    }
+
+    public void sort(){
+        quicksort(data, start, end);
+    }
+
+    private void quicksort(int[] data, int start, int end) {
+        if(start<end){
+            int position = partition(data, start, end);
+            quicksort(data,start,position);
+            quicksort(data,position+1, end);
+        }
+    }
+
+    private int partition(int[] data, int start, int end){
+        int i = start;
+        int j = end;
+        int pivot = data[start];
+        j--;
+        while(i<j){
+            while (data[j] > pivot && i < j){
+                j--;
+            }
+            if (i < j) {
+                data[i] = data[j];
+                i++;
+            }
+            while(data[i] <= pivot && i < j){
+                i++;
+            }
+            if (i<j) {
+                data[j] = data[i];
+                j--;
+            }
+        }
+        data[i] = pivot;
+        return i;
+    }
+
+    public void print(){
+        for(int i = 0; i < data.length; i++){
+            System.out.println(data[i]);
+        }
+    }
+}
