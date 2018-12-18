db7411fac81e65cda77dcf033ce03c5e35430547
lucifer <psinghal4210@gmail.com>
2018-12-19 02:12:06 +0530

    Stack: Using linkedlist JDK

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -5,14 +5,14 @@ import linkedlist.Employee;
 public class Main {
 
     public static void main(String[] args) {
-        StackLinkedList stackArray = new StackLinkedList();
+        StackJDK stackArray = new StackJDK();
         Employee charlie = new Employee("Charlie", "John", 123);
         Employee alpha = new Employee("Alpha", "Man", 234);
         Employee sara = new Employee("Sara", "Khan", 32);
         stackArray.push(charlie);
         stackArray.push(alpha);
         stackArray.push(sara);
-        System.out.println(stackArray.peak());
+        System.out.println(stackArray.peek());
         System.out.println(stackArray.pop());
         stackArray.print();
     }
---
+++ src/stack/StackJDK.java
@@ -0,0 +1,35 @@
+package stack;
+
+import linkedlist.Employee;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class StackJDK {
+
+    private Deque<Employee> stack;
+
+    StackJDK() {
+        stack = new LinkedList<>();
+    }
+
+    public void push(Employee employee) {
+        stack.addFirst(employee);
+    }
+
+    public Employee pop() {
+        return stack.removeFirst();
+    }
+
+    public Employee peek() {
+        return stack.peek();
+    }
+
+    public void print() {
+        Iterator itr = stack.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
