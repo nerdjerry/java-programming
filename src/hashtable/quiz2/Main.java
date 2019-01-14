b3cbc4ecbfdc0cbc01b453ef977b8fcefcf87dad
lucifer <psinghal4210@gmail.com>
2019-01-14 22:55:27 +0530

    Hashtable : Quizes

---
+++ src/hashtable/quiz2/Main.java
@@ -0,0 +1,44 @@
+package hashtable.quiz2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList<Employee> employees = new LinkedList<>();
+        employees.add(new Employee("Jane", "Jones", 123));
+        employees.add(new Employee("John", "Doe", 5678));
+        employees.add(new Employee("Mike", "Wilson", 45));
+        employees.add(new Employee("Mary", "Smith", 5555));
+        employees.add(new Employee("John", "Doe", 5678));
+        employees.add(new Employee("Bill", "End", 3948));
+        employees.add(new Employee("Jane", "Jones", 123));
+
+        employees.forEach(e -> System.out.println(e));
+        HashMap<Integer, Employee> hashmap = new HashMap<>();
+        employees.forEach(e -> hashmap.putIfAbsent((Integer) e.getId(), e));
+        employees.clear();
+        Iterator<Employee> iterator = hashmap.values().iterator();
+        while (iterator.hasNext()) {
+            employees.add(iterator.next());
+        }
+        System.out.println("Unique employees");
+        employees.forEach(e -> System.out.println(e));
+
+//        int[] nums = new int[10];
+//        int[] numsToAdd = { 59382, 43, 6894, 500, 99, -58 };
+//        for (int i = 0; i < numsToAdd.length; i++) {
+//            nums[hash(numsToAdd[i])] = numsToAdd[i];
+//        }
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.println(nums[i]);
+//        }
+    }
+
+    public static int hash(int value) {
+        return Math.abs(value % 10);
+    }
+}
