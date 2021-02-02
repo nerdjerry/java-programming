e7a441562d9a55293c1e345a823c60a43d202161
lucifer <psinghal4210@gmail.com>
2021-02-02 17:19:19 +0530

    Algorith(Sort) : Bubble Sort

---
+++ src/sort/BubbleSortAlgo.java
@@ -0,0 +1,19 @@
+package sort;
+
+public class BubbleSortAlgo {
+    public static void sort(int[] array){
+        for(int i=0; i < array.length; i++){
+            for(int j = 0; j < array.length - i - 1; j++){
+                if(array[j] > array[j+1]){
+                    swap(array, j, j+1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] array, int j, int k){
+        int temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+    }
+}
