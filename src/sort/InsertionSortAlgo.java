eb7e24adf6feac0540fdfe6b38faab5c2a6e439c
lucifer <psinghal4210@gmail.com>
2021-02-02 17:19:50 +0530

    Algorith(Sort) : Insertion Sort

---
+++ src/sort/InsertionSortAlgo.java
@@ -0,0 +1,15 @@
+package sort;
+
+public class InsertionSortAlgo {
+
+    public static void sort(int[] input){
+        for(int firstUnsortedIndex = 1; firstUnsortedIndex < input.length; firstUnsortedIndex++){
+            int newElement = input[firstUnsortedIndex];
+            int i = firstUnsortedIndex;
+            for(; i > 0 && input[i-1] > newElement;i--){
+                input[i] = input[i-1];
+            }
+            input[i] = newElement;
+        }
+    }
+}
