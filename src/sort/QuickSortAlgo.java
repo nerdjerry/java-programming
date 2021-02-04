26be5adc85e1a9e129fa75968eb465621cb3d15f
lucifer <psinghal4210@gmail.com>
2021-02-04 10:14:52 +0530

    Algorithm(Sort) : Quick Sort

---
+++ src/sort/QuickSortAlgo.java
@@ -0,0 +1,40 @@
+package sort;
+
+public class QuickSortAlgo {
+
+    public static void sort(int[] array, int start, int end){
+        if(start < end){
+            int position = partition(array, start, end);
+            sort(array,start, position);
+            sort(array,position+1, end);
+        }
+    }
+
+    public static int partition(int[] array, int start, int end){
+        int left = start;
+        int right = end-1;
+        int pivot = array[left];
+
+        while(left < right){
+            while(array[right] > pivot && left < right){
+                right--;
+            }
+
+            if(left < right){
+                array[left] = array[right];
+                left++;
+            }
+
+            while(array[left] < pivot && left < right){
+                left++;
+            }
+
+            if(left < right){
+                array[right] = array[left];
+                right--;
+            }
+        }
+        array[right] = pivot;
+        return right;
+    }
+}
